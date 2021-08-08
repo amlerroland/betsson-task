232ccf7e91e35289ee2bade7a8c68216ed4e3089
Amler Roland <amlerroland@gmail.com>
2021-08-08 16:45:32 +0200

    rotues and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompetitionListComponent } from './competition-list/competition-list.component';
+import { CompetitionDetailComponent } from './competition-detail/competition-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MatchComponent } from './match/match.component';
+import { MatchFullComponent } from './match/match-full.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CompetitionListComponent },
+  { path: 'competitions/:id', component: CompetitionDetailComponent },
+  { path: 'matches/:id', component: MatchComponent },
+  { path: 'match-full', component: MatchFullComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CompetitionListComponent } from './competition-list/competition-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CompetitionDetailComponent } from './competition-detail/competition-detail.component';
+import { AreaComponent } from './area/area.component';
+import { MatchComponent } from './match/match.component';
+import { MatchFullComponent } from './match/match-full.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    CompetitionListComponent,
+    CompetitionDetailComponent,
+    AreaComponent,
+    MatchComponent,
+    MatchFullComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
